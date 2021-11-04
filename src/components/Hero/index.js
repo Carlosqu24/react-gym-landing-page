c7e15caa1d1fafe3fc9613d8b28ced350bd3a7f6
Carlos Quesada <carlosquesadaumana@gmail.com>
2021-11-03 20:10:56 -0600

    Hero fixed

--- src/components/Hero/index.js
+++ src/components/Hero/index.js
@@ -8,10 +8,12 @@ export const Hero = () => {
       return (
             <div id="hero" className="hero">
                   <div className="hero__overlay">
+                        <div className="container">
                         <div className="hero-card">
                               <h2 className="hero-title">¡Siente el poder!</h2>
                               <p className="hero-description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aperiam, cumque molestias? Minima doloribus officiis nisi vel ea, corrupti sapiente molestias?</p>
                               <a href="" className="hero-link">Ver más</a>
+                        </div>
                         </div>
                   </div>
             </div>
